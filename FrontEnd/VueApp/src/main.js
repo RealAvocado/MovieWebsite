8878b42f6d8f68f97f7d9eede418ce128223479f
RealAvocado <Letian.Jiang1106@outlook.com>
2023-04-11 18:37:22 +0100

    Vue project in the beginning. Now values of Genre and Year can be selected by clicking buttons.

--- FrontEnd/VueApp/src/main.js
+++ FrontEnd/VueApp/src/main.js
@@ -3,14 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// include Bootstrap in the project
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+import 'bootstrap/dist/js/bootstrap.js'
 
 Vue.config.productionTip = false
 
